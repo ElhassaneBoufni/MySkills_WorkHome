9afb28d2933bf74d076ae4aa61b0184910f01427
ElhassaneBoufni <el-hassane.boufni@capgemini.com>
2019-04-08 16:11:24 +0100

    modif style compETcertif

--- MySkills/MySkills/ClientApp/src/app/compETcertif/competence-view/competence-view.component.ts
+++ MySkills/MySkills/ClientApp/src/app/compETcertif/competence-view/competence-view.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-competence-view',
@@ -11,31 +12,41 @@ export class CompetenceViewComponent implements OnInit {
   removable = true;
   displayed = true;
 
-  CompFound = [
-    'Asp.net',
-    'Asp.net MVC',
-    'Java EE',
-    'Spring MVC',
-    'Struts 2',
-    'Angular 7',
-    'Dynamics CRM',
-    'SalesForce',
-    'SQL Server Integration Services',
-    'PHP',
-    'Laravel',
-    'Aprimo',
-    'CPQ (Big machines)',
-    'Computer Tele Integration-CTI',
-    'OSL Marketing Cloud (Eloqua)',
-    'MFG/PRO',
-    'Oracle CRM On Demand',
-    'Hybris',
-    'CRM – Others',
+  CompFound: { name: string, recommended: number }[] = [
+    { 'name': 'Asp.net', 'recommended': 0 },
+    { 'name': 'Asp.net MVC', 'recommended': 1 },
+    { 'name': 'Java EE', 'recommended': 0 },
+    { 'name': 'Spring MVC', 'recommended': 0 },
+    { 'name': 'Struts 2', 'recommended': 0 },
+    { 'name': 'Angular 7', 'recommended': 1 },
+    { 'name': 'Dynamics CRM', 'recommended': 0 },
+    { 'name': 'SalesForce', 'recommended': 0 },
+    { 'name': 'SQL Server Integration Services', 'recommended': 0 },
+    { 'name': 'PHP', 'recommended': 0 },
+    { 'name': 'Laravel', 'recommended': 0 },
+    { 'name': 'Aprimo', 'recommended': 0 },
+    { 'name': 'CPQ (Big machines)', 'recommended': 0 },
+    { 'name': 'Computer Tele Integration-CTI', 'recommended': 0 },
+    { 'name': 'OSL Marketing Cloud (Eloqua)', 'recommended': 0 },
+    { 'name': 'MFG/PRO', 'recommended': 0 },
+    { 'name': 'Oracle CRM On Demand', 'recommended': 0 },
+    { 'name': 'Hybris', 'recommended': 0 },
+    { 'name': 'CRM – Others', 'recommended': 0 }
   ];
 
-  CompSelected = [
-    'Asp.net Core 2.2'
+  CompSelected: { name: string, recommended?: number }[] = [
+    { 'name': 'Asp.net Core 2.2'}
   ];
+  constructor() {
+    if (this.CompSelected.length <= 0) {
+      this.displayed = true;
+    } else {
+      this.displayed = false;
+    }
+  }
+
+  ngOnInit() {
+  }
 
   drop(event: CdkDragDrop<string[]>) {
     if (event.previousContainer === event.container) {
@@ -57,7 +68,7 @@ export class CompetenceViewComponent implements OnInit {
     const index = this.CompSelected.indexOf(comp);
 
     if (index >= 0) {
-      transferArrayItem(this.CompSelected, this.CompFound, this.CompSelected.indexOf(comp), this.CompFound.length);
+      transferArrayItem(this.CompSelected, this.CompFound , this.CompSelected.indexOf(comp), this.CompFound.length);
       if (this.CompSelected.length <= 0) {
         this.displayed = true;
       } else {
@@ -67,15 +78,9 @@ export class CompetenceViewComponent implements OnInit {
       //https://xd.adobe.com/spec/0c0274ae-b6f0-45b5-6132-e74e223d39e1-7a33/screen/bae4dae5-131f-4068-8af8-ccb36566ba9c/Web-1920-3/
     }
   }
-  constructor() {
-    if (this.CompSelected.length <= 0) {
-      this.displayed = true;
-    } else {
-      this.displayed = false;
-    }
-  }
 
-  ngOnInit() {
+  onSubmit(form: NgForm){
+    console.log(form.value);
   }
 
 }
